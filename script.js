d45c52704bc0734f815fd4000e6dce77210d3273
Matheusp-Silva <matheusp.silva@outlook.com.br>
2025-03-31 09:31:03 -0300

    Criado a landing page com html, css e js para a empresa olimpia guincho, os reviews foram colocados manualmente podendo no futuro ser via API.

---
+++ script.js
@@ -0,0 +1,15 @@
+// Inicializa o carrossel
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
